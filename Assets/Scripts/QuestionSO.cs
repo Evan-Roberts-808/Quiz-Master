800c51ab128197c77a9bf13865bc683662078305
Evan Roberts <eroberts1998@icloud.com>
2024-01-18 11:21:24 -0500

    add getters for question scritable object

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -7,11 +7,12 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2, 6)]
     [SerializeField] string question = "Enter new question";
-    [SerializeField] string[] answers = {"Enter your answers"};
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswer;
 
-    public string GetQuestion()
-    {
-        return question;
-    }
+    public string GetQuestion() { return question; }
 
+    public int GetCorrectAnswerIndex() { return correctAnswer; }
+
+    public string GetAnswer(int index) { return answers[index]; }
 }
